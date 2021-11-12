1ec9cb946fefb20c0d2c44d7174b432e4f9f1a71
LeilaSchooley <vortex962@zoho.com>
2021-11-12 16:35:39 +0000

    changes

--- index.js
+++ index.js
@@ -14,18 +14,23 @@ function resetAllImages() {
 let previousButton = document.getElementById("previous");
 previousButton.addEventListener("click", () => {
   resetAllImages();
+
   if (current === 0) {
     current = galleryImages.length;
   }
   galleryImages[current - 1].style.display = "block";
+
+  current--;
 });
+
 let nextButton = document.getElementById("next");
 nextButton.addEventListener("click", () => {
   resetAllImages();
-  if (current === galleryImages.length - 1) {
-    current = -1;
+  if (current == galleryImages.length - 1) {
+    current == -1;
   }
   galleryImages[current + 1].style.display = "block";
+  current++;
 });
 
 initializeGallery();
